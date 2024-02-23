022ad95b89a4c97ef9eb569e2da554aea8121e92
fjs <jiansen.fu@chinacreator.com>
2024-02-23 15:29:21 +0800

    chore: 文件补充

--- mirageLedger-common/src/main/java/com/jason/mirageledger/common/CustomExceptionHandler.java
+++ mirageLedger-common/src/main/java/com/jason/mirageledger/common/CustomExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.jason.mirageledger.common;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -17,6 +19,19 @@ public class CustomExceptionHandler {
         body.put("message", ex.getMessage());
 
         return new ResponseEntity<>(body, ex.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGeneralException(Exception ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        body.put("message", "内部服务器错误，请联系管理员");
+
+        String path = request.getDescription(false);
+        body.put("path", path);
+        body.put("error", ex.getClass().getSimpleName());
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
 
