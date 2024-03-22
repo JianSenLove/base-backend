eb2d99e00f600bde312875b2eda3c64d9fdbf63c
JianSenLove <1063997637@qq.com>
2024-03-22 16:51:43 +0800

    chore: 项目初始化

--- db/mysql_ddl_v1.sql
+++ db/mysql_ddl_v1.sql
@@ -1,32 +1,57 @@
-DROP TABLE IF EXISTS `mirageledger_user`;
-CREATE TABLE `mirageledger_user`  (
-                                      `ID_` varchar(64)   NOT NULL COMMENT 'ID',
-                                      `NAME_` varchar(255)   NOT NULL COMMENT '用户名称',
-                                      `CODE_` varchar(64)   NOT NULL COMMENT '登录账号',
-                                      `DESC_` varchar(255)   NULL DEFAULT NULL COMMENT '用户描述',
-                                      `PASSWORD_` varchar(64)   NOT NULL COMMENT '账号密码',
-                                      `CREATE_TIME` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-                                      `UPDATE_TIME` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+CREATE TABLE mirageledger_user (
+                                   ID_ varchar(64) PRIMARY KEY NOT NULL COMMENT 'ID',
+                                   NAME_ varchar(255) NOT NULL COMMENT '用户名称',
+                                   CODE_ varchar(64) NOT NULL COMMENT '登录账号',
+                                   DESC_ varchar(255) NULL DEFAULT NULL COMMENT '用户描述',
+                                   PASSWORD_ varchar(64) NOT NULL COMMENT '账号密码',
+                                   CREATE_TIME_ timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+                                   UPDATE_TIME_ timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) COMMENT = '用户信息表';
-INSERT INTO `mirageledger_user` (`ID_`, `NAME_`, `CODE_`, `DESC_`, `PASSWORD_`, `CREATE_TIME`, `UPDATE_TIME`) VALUES ('admin', '教务处管理员', 'admin', '管理账户', 'admin123', '2024-02-01 16:03:40', '2024-02-01 16:03:45');
 
-DROP TABLE IF EXISTS `mirageledger_course`;
-CREATE TABLE `mirageledger_course`  (
-                                        `ID_` varchar(64)   NOT NULL COMMENT 'ID',
-                                        `NAME_` varchar(255)   NOT NULL COMMENT '课程名称',
-                                        `USER_ID` varchar(64)   NOT NULL COMMENT '用户ID',
-                                        `TERM_` varchar(64)   NOT NULL COMMENT '学期',
-                                        `DESC_` varchar(255)   NULL DEFAULT NULL COMMENT '课程描述',
-                                        `CREATE_TIME` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-                                        `UPDATE_TIME` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
-) COMMENT = '课程表';
+CREATE TABLE mirageledger_product (
+                                      ID_ varchar(64) PRIMARY KEY NOT NULL COMMENT 'ID',
+                                      NAME_ varchar(255) NOT NULL COMMENT '商品名称',
+                                      CATEGORY_ID varchar(64) NOT NULL COMMENT '类别ID',
+                                      PRICE_ double NOT NULL COMMENT '商品价格',
+                                      STOCK int NOT NULL COMMENT '商品库存',
+                                      USER_ID varchar(64) NOT NULL COMMENT '用户ID',
+                                      DESC_ varchar(255) NULL DEFAULT NULL COMMENT '商品描述',
+                                      CREATE_TIME_ timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+                                      UPDATE_TIME_ timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+) COMMENT = '商品信息表';
 
-DROP TABLE IF EXISTS `mirageledger_course_evaluation`;
-CREATE TABLE `mirageledger_course_evaluation`  (
-                                                   `ID_` varchar(64)   NOT NULL COMMENT 'ID',
-                                                   `COURSE_ID` varchar(64)   NOT NULL  COMMENT '课程ID',
-                                                   `TYPE_` varchar(16)   NOT NULL  COMMENT '评价类型',
-                                                   `SCORE_` DOUBLE NULL DEFAULT NULL COMMENT '课程评价分',
-                                                   `CREATE_TIME` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-                                                   `UPDATE_TIME` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
-) COMMENT = '课程评分表';
+CREATE TABLE mirageledger_cart (
+                                   ID_ varchar(64) PRIMARY KEY NOT NULL COMMENT 'ID',
+                                   USER_ID varchar(64) NOT NULL COMMENT '用户ID',
+                                   PRODUCT_ID varchar(64) NOT NULL COMMENT '商品ID',
+                                   QUANTITY_ int NOT NULL COMMENT '数量',
+                                   CREATE_TIME timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+                                   UPDATE_TIME timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+) COMMENT = '用户购物车表';
+
+CREATE TABLE mirageledger_view_history (
+                                           ID_ varchar(64) PRIMARY KEY NOT NULL COMMENT 'ID',
+                                           USER_ID varchar(64) NOT NULL COMMENT '用户ID',
+                                           PRODUCT_ID varchar(64) NOT NULL COMMENT '商品ID',
+                                           CREATE_TIME_ timestamp NOT NULL COMMENT '创建时间'
+) COMMENT = '用户商品浏览记录表';
+
+CREATE TABLE mirageledger_order (
+                                    ID_ varchar(64) PRIMARY KEY NOT NULL COMMENT 'ID',
+                                    USER_ID varchar(64) NOT NULL COMMENT '用户ID',
+                                    ORDER_PRICE double NOT NULL COMMENT '订单总价',
+                                    STATUS_ varchar(64) NOT NULL COMMENT '订单状态',
+                                    CREATE_TIME timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+                                    UPDATE_TIME timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+) COMMENT = '订单表';
+
+CREATE TABLE mirageledger_order_product (
+                                            ID_ varchar(64) PRIMARY KEY NOT NULL COMMENT 'ID',
+                                            ORDER_ID varchar(64) NOT NULL COMMENT '订单ID',
+                                            PRODUCT_ID varchar(64) NOT NULL COMMENT '商品ID',
+                                            QUANTITY_ int NOT NULL COMMENT '数量',
+                                            PRICE_ double NOT NULL COMMENT '商品单价',
+                                            TOTAL_PRICE double NOT NULL COMMENT '商品总价',
+                                            CREATE_TIME timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+                                            UPDATE_TIME timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+) COMMENT = '订单商品关联表';
