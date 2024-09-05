6a1bdefd5b2fac9101a97d5f297a3dfac4d2c4c0
JianSenFu <1063997637@qq.com>
2024-09-05 17:49:46 +0800

    feat: 教师接口

--- moneybag-teacher/src/main/java/com/jason/moneybag/teacher/controller/TeacherController.java
+++ moneybag-teacher/src/main/java/com/jason/moneybag/teacher/controller/TeacherController.java
@@ -1,11 +1,17 @@
 package com.jason.moneybag.teacher.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jason.moneybag.AuthenticationUtil;
+import com.jason.moneybag.DefaultImageEnum;
 import com.jason.moneybag.JwtTokenUtil;
+import com.jason.moneybag.RestPreconditions;
 import com.jason.moneybag.teacher.entity.Teacher;
 import com.jason.moneybag.teacher.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,97 +25,80 @@ public class TeacherController {
     private JwtTokenUtil jwtTokenUtil;
 
     @PostMapping("/login")
-    public String TeacherLogin(@RequestBody Teacher Teacher) {
+    public String TeacherLogin(@RequestBody Teacher teacher) {
 
-//        RestPreconditions.checkParamArgument(StringUtils.isNotBlank(Teacher.getCode()) || StringUtils.isNotBlank(Teacher.getPassword()), "账号或密码为空!");
-//
-//        Teacher one = TeacherService.getOne(new LambdaQueryWrapper<Teacher>().eq(Teacher::getCode, Teacher.getCode()));
+        RestPreconditions.checkParamArgument(StringUtils.isNotBlank(teacher.getUseraccount()) || StringUtils.isNotBlank(teacher.getPassword()), "账号或密码不能为空!");
 
-//        RestPreconditions.checkParamArgument(one != null, "账号不存在");
-//
-//        RestPreconditions.checkParamArgument(one.getPassword().equals(Teacher.getPassword()), "密码不正确");
-//
-//        final String token = jwtTokenUtil.generateToken(one.getId());
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("token", token);
-//        map.put("TeacherInfo", one);
-//        return map;
-        return "login";
+        Teacher one = teacherService.getOne(new LambdaQueryWrapper<Teacher>().eq(Teacher::getUseraccount, teacher.getUseraccount()));
+
+        RestPreconditions.checkParamArgument(one != null, "账号不存在");
+
+        RestPreconditions.checkParamArgument(one.getPassword().equals(teacher.getPassword()), "密码不正确");
+
+        return jwtTokenUtil.generateToken(one.getId());
     }
 
     @PostMapping("/register")
-    public Teacher TeacherRegister(@RequestBody Teacher Teacher) {
+    public Teacher TeacherRegister(@RequestBody Teacher teacher) {
+        RestPreconditions.checkParamArgument(StringUtils.isNotBlank(teacher.getUseraccount()), "账号不能为空!");
+        RestPreconditions.checkParamArgument(StringUtils.isNotBlank(teacher.getName()), "用户名不能为空!");
+        RestPreconditions.checkParamArgument(StringUtils.isNotBlank(teacher.getPassword()), "密码不能为空!");
 
-//        RestPreconditions.checkParamArgument(AuthenticationUtil.isAdmin(), "只有管理员能进行操作");
-//
-//        RestPreconditions.checkParamArgument(StringUtils.isNotBlank(Teacher.getCode()), "账号不能为空!");
-//        RestPreconditions.checkParamArgument(StringUtils.isNotBlank(Teacher.getName()), "用户名不能为空!");
-//        RestPreconditions.checkParamArgument(StringUtils.isNotBlank(Teacher.getPassword()), "密码不能为空!");
-//
-//        Teacher existTeacher = TeacherService.getOne(new LambdaQueryWrapper<Teacher>().eq(Teacher::getCode, Teacher.getCode()));
-//        RestPreconditions.checkParamArgument(existTeacher == null, "账号已存在!");
-//
-//        TeacherService.save(Teacher);
+        Teacher existTeacher = teacherService.getOne(new LambdaQueryWrapper<Teacher>().eq(Teacher::getUseraccount, teacher.getUseraccount()));
+        RestPreconditions.checkParamArgument(existTeacher == null, "账号已存在!");
+
+        // 配置默认头像
+        teacher.setAvatar(DefaultImageEnum.AVATAR.getimageBase64());
+
+        // TODO 配置默认系统信息背景
+
+        teacherService.save(teacher);
         return null;
     }
 
     @PutMapping("/{id}")
     public Teacher updateTeacher(@PathVariable("id") String id, @RequestBody Teacher Teacher) {
 
-//        RestPreconditions.checkParamArgument(id.equals(AuthenticationUtil.getAuthentication()) || AuthenticationUtil.isAdmin(), "只能修改自己账户的密码", HttpStatus.FORBIDDEN);
-//
-//        Teacher checkIdTeacher = TeacherService.getById(id);
-//        RestPreconditions.checkParamArgument(checkIdTeacher != null, "用户不存在!");
-//
-//        if (StringUtils.isBlank(Teacher.getName())) Teacher.setName(null);
-//        if (StringUtils.isBlank(Teacher.getPassword())) Teacher.setPassword(null);
-//        Teacher.setId(id);
-//        TeacherService.updateById(Teacher);
-//        Teacher.setImage(baseImagePath + Teacher.getId() + ".jpg");
+        RestPreconditions.checkParamArgument(id.equals(AuthenticationUtil.getAuthentication()) || AuthenticationUtil.isAdmin(), "只能修改自己账户的密码", HttpStatus.FORBIDDEN);
+
+        Teacher checkIdTeacher = teacherService.getById(id);
+        RestPreconditions.checkParamArgument(checkIdTeacher != null, "用户不存在!");
+
+        if (StringUtils.isBlank(Teacher.getName())) Teacher.setName(null);
+        if (StringUtils.isBlank(Teacher.getPassword())) Teacher.setPassword(null);
+        Teacher.setId(id);
+        teacherService.updateById(Teacher);
         return null;
     }
 
     @DeleteMapping("/{id}")
     public void deleteTeacher(@PathVariable("id") String id) {
-//        RestPreconditions.checkParamArgument(AuthenticationUtil.isAdmin(), "只有管理员能进行操作");
-//        TeacherService.removeById(id);
+        RestPreconditions.checkParamArgument(AuthenticationUtil.isAdmin(), "只有管理员能进行操作");
+        teacherService.removeById(id);
     }
 
     @GetMapping("/{id}")
-    public String getTeacher(@PathVariable("id") String id) {
-//        RestPreconditions.checkParamArgument(id.equals(AuthenticationUtil.getAuthentication()) || AuthenticationUtil.isAdmin(), "只能查看自己的账户信息", HttpStatus.FORBIDDEN);
-//
-//        Teacher Teacher = TeacherService.getById(id);
-//        RestPreconditions.checkParamArgument(Teacher != null, "用户不存在!");
-//        Teacher.setImage(baseImagePath + Teacher.getId() + ".jpg");
-        return "666";
+    public Teacher getTeacher(@PathVariable("id") String id) {
+        RestPreconditions.checkParamArgument(id.equals(AuthenticationUtil.getAuthentication()) || AuthenticationUtil.isAdmin(), "只能查看自己的账户信息", HttpStatus.FORBIDDEN);
+
+        Teacher Teacher = teacherService.getById(id);
+        RestPreconditions.checkParamArgument(Teacher != null, "用户不存在!");
+        return null;
     }
 
     @GetMapping("")
     public Page<Teacher> getTeacherPage(@RequestParam(defaultValue = "1") Integer page,
                                        @RequestParam(defaultValue = "10") Integer rows,
                                   @RequestParam(required = false) String name) {
-//
-//        RestPreconditions.checkParamArgument(AuthenticationUtil.isAdmin(), "只有管理员能进行操作");
-//
-//        Page<Teacher> TeacherPage = new Page<>(page, rows);
-//        LambdaQueryWrapper<Teacher> TeacherLambdaQueryWrapper = new LambdaQueryWrapper<>();
-//        if (StringUtils.isNotBlank(name)) {
-//            TeacherLambdaQueryWrapper.like(Teacher::getName, name);
-//        }
-//
-//        TeacherLambdaQueryWrapper.orderByDesc(Teacher::getUpdateTime);
-//        TeacherService.page(TeacherPage,TeacherLambdaQueryWrapper);
-//        TeacherPage.getRecords().forEach(Teacher -> Teacher.setImage(baseImagePath + Teacher.getId() + ".jpg"));
-        return null;
-    }
 
-    //根据code获取用户接口
-    @GetMapping("/code/{code}")
-    public Teacher getTeacherByCode(@PathVariable("code") String code) {
-//        Teacher Teacher = TeacherService.getOne(new LambdaQueryWrapper<Teacher>().eq(Teacher::getCode, code));
-//        RestPreconditions.checkParamArgument(Teacher != null, "用户不存在!");
-//        RestPreconditions.checkParamArgument(Teacher.getId().equals(AuthenticationUtil.getAuthentication()) || AuthenticationUtil.isAdmin(), "只能查看自己的账户信息", HttpStatus.FORBIDDEN);
-        return null;
+        RestPreconditions.checkParamArgument(AuthenticationUtil.isAdmin(), "只有管理员能进行操作");
+
+        Page<Teacher> TeacherPage = new Page<>(page, rows);
+        LambdaQueryWrapper<Teacher> teacherLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        if (StringUtils.isNotBlank(name)) {
+            teacherLambdaQueryWrapper.like(Teacher::getName, name);
+        }
+
+        return teacherService.page(TeacherPage,teacherLambdaQueryWrapper);
     }
 }
